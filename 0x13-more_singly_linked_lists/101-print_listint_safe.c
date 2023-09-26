e0b5857fc8e2efc66c112272659400af7109e18f
MartinMousa <martin.mousa88@gmail.com>
2023-09-26 13:59:17 -0700

    0x13 tasks

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -15,28 +15,38 @@ size_t print_listint_safe(const listint_t *head);
 size_t looped_listint_len(const listint_t *head)
 {
 	const listint_t *tortoise, *hare;
-	size_t nodes = 0;
+	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
 
-	tortoise = head;
-	hare = head;
+	tortoise = head->next;
+	hare = (head->next)->next;
 
-	while (hare && hare->next)
+	while (hare)
 	{
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-
 		if (tortoise == hare)
 		{
-			do {
+			tortoise = head;
+			while (tortoise != hare)
+			{
 				nodes++;
 				tortoise = tortoise->next;
-			} while (tortoise != hare);
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
 
 			return (nodes);
 		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
 
 	return (0);
@@ -50,31 +60,28 @@ size_t looped_listint_len(const listint_t *head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-	const listint_t *current = head;
+	size_t nodes, index = 0;
 
 	nodes = looped_listint_len(head);
 
 	if (nodes == 0)
 	{
-		while (current != NULL)
+		for (; head != NULL; nodes++)
 		{
-			printf("[%p] %d\n", (void *)current, current->n);
-			current = current->next;
-			nodes++;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
+
 	else
 	{
-		size_t i;
-		for (i = 0; i < nodes; i++)
+		for (index = 0; index < nodes; index++)
 		{
-			printf("[%p] %d\n", (void *)current, current->n);
-			current = current->next;
-			nodes++;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 
-		printf("-> [%p] %d\n", (void *)current, current->n);
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
 	return (nodes);
