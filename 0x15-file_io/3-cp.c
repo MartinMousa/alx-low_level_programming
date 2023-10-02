dccd9a16ac65ea4cd5570a0141dfb6611619e550
MartinMousa <martin.mousa88@gmail.com>
2023-10-02 15:30:12 -0700

    test file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 char *create_buffer(char *file);
-void close_file(int dc);
+void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
@@ -13,33 +13,33 @@ void close_file(int dc);
  */
 char *create_buffer(char *file)
 {
-	char *bfr;
+	char *buffer;
 
-	bfr = malloc(sizeof(char) * 1024);
+	buffer = malloc(sizeof(char) * 1024);
 
-	if (bfr == NULL)
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
-	return (bfr);
+	return (buffer);
 }
 
 /**
  * close_file - Closes file descriptors.
- * @dc: The file descriptor to be closed.
+ * @fd: The file descriptor to be closed.
  */
-void close_file(int dc)
+void close_file(int fd)
 {
-	int z;
+	int c;
 
-	z = close(dc);
+	c = close(fd);
 
-	if (z == -1)
+	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close dc %d\n", dc);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -58,46 +58,46 @@ void close_file(int dc)
  */
 int main(int argc, char *argv[])
 {
-	int fmor, xo, i, w;
-	char *bfr;
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_fmor file_xo\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	bfr = create_buffer(argv[2]);
-	fmor = open(argv[1], O_RDONLY);
-	i = read(fmor, bfr, 1024);
-	xo = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (fmor == -1 || i == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
-			free(bfr);
+			free(buffer);
 			exit(98);
 		}
 
-		w = write(xo, bfr, i);
-		if (xo == -1 || w == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write xo %s\n", argv[2]);
-			free(bfr);
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
 			exit(99);
 		}
 
-		i = read(fmor, bfr, 1024);
-		xo = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (i > 0);
+	} while (r > 0);
 
-	free(bfr);
-	close_file(fmor);
-	close_file(xo);
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
