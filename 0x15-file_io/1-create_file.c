ef16ae4761244be42c2c01a5b5fd21f7cb68f077
MartinMousa <martin.mousa88@gmail.com>
2023-10-02 14:57:55 -0700

    test file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
- * createfile - Createsfile.
+ * create file - Creates file.
  * @flnm: point name of file for creatation.
- * @txt__cont: point le str write file.
+ * @txt_cot: point le str write file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
-int create_file(const char *flnm, char *txt__cont)
+int create_file(const char *flnm, char *txt_cot)
 {
-	int fwd, wxw, berlen = 0;
+	int df, x, bl = 0;
 
 	if (flnm == NULL)
 		return (-1);
 
-	if (txt__cont != NULL)
+	if (txt_cot != NULL)
 	{
-		for (berlen = 0; txt__cont[berlen];)
-			berlen++;
+		for (bl = 0; txt_cot[bl];)
+			bl++;
 	}
 
-	fwd = open(flnm, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wxw = write(fwd, txt__cont, berlen);
+	df = open(flnm, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	x = write(df, txt_cot, bl);
 
-	if (fwd == -1 || wxw == -1)
+	if (df == -1 || x == -1)
 		return (-1);
 
-	close(fwd);
+	close(df);
 
 	return (1);
 }
